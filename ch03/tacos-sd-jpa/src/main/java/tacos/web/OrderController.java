a2fd01ae4bb45ffc051360bbae7d4066032f64df
PierreSQS <nomo_messina@web.de>
2021-01-22 20:06:25 +0100

    Commit ch03-03 OK: Made the DesignController more readable and removed
    unused code

--- ch03/tacos-sd-jpa/src/main/java/tacos/web/OrderController.java
+++ ch03/tacos-sd-jpa/src/main/java/tacos/web/OrderController.java
@@ -2,7 +2,6 @@ package tacos.web;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
